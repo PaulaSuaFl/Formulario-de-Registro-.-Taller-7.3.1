f69099409aafb98a8c3e419ead64c5e65114a01b
PaulaSuaFl <paulasuafl@gmail.com>
2024-09-01 19:35:55 -0300

    Co-authored-by: valyazrochizq@hotmail.com <valyazrochizq@hotmail.com> Co-authored-by: Fitzroyfang <Fitzroyfang@users.noreply.github.com> Co-authored-by: smoonlighth <sabrinahernmull@gmail.com>

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+})
+
+document.getElementById("regBtn").addEventListener('click',()=>{
+
+let nombre = document.getElementById("nombre").value;
+let apellido = document.getElementById("apellido").value;
+let email = document.getElementById("email").value;
+let contraseña = document.getElementById("password1").value;
+let contraseña2 = document.getElementById("password2").value;
+let checkbox = document.getElementById("terminos");
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +18,9 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+if (nombre!='' && apellido!='' && email!='' && contraseña!='' && contraseña2!='' &&  contraseña.length >= 6 && contraseña === contraseña2 && checkbox.checked) {
+  showAlertSuccess();
+} else {
+    showAlertError();
+}
+})
